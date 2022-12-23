c7f3d98f386c2bd093e35f1bf8dc201b1f99481f
confusedme6 <aljpc+confusedme6@pm.me>
2022-12-23 17:20:01 +0100

    added nvim-cmp, luasnip

--- lua/config/lazy/plugins.lua
+++ lua/config/lazy/plugins.lua
@@ -3,8 +3,11 @@
 -- (or one that groups all the color schemes for example)
 -- and then they all get merged into one in this file
 return {
+    -- fuzzyfinder | telescope
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
+
+    -- themes
     {
         "rose-pine/neovim",
 	name = "rose-pine",
@@ -12,6 +15,8 @@ return {
     "rebelot/kanagawa.nvim",
     "Yazeed1s/minimal.nvim",
     "sainnhe/sonokai",
+
+    -- ast | treesitter + helper plugins
     {
     "nvim-treesitter/nvim-treesitter",
     config = function () 
@@ -20,7 +25,11 @@ return {
     },
     "nvim-treesitter/playground",
     "mbbill/undotree",
+
+    -- git client | fugitive
     "tpope/vim-fugitive",
+
+    -- lsp integration | mason + lsp config plugins
     {
     "williamboman/mason.nvim",
     lazy = false,
@@ -41,4 +50,20 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     },
+
+    -- autocompletion | nvim-cmp 
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+
+    -- autocompletion | completion sources
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lua",
+    "saadparwaiz1/cmp_luasnip",
+
+    -- snippets | luasnip
+    "L3MON4D3/LuaSnip",
+    "rafamadriz/friendly-snippets"
+
 }
