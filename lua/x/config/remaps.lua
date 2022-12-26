2fa059c3c20047fecf4e59b393e165d7d2acb6e9
confusedme6 <aljpc+confusedme6@pm.me>
2022-12-27 00:27:52 +0100

    more mappings

--- lua/x/config/remaps.lua
+++ lua/x/config/remaps.lua
@@ -10,5 +10,21 @@ vim.keymap.set("n", "<leader>lzy", vim.cmd.Lazy)
 vim.keymap.set("n", "<leader>msn", vim.cmd.Mason)
 
 -- move visually highlighted text
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+-- todo: should add the equivalent for H & L mapping to < and >
+
+-- when using J keep the cursor at the start of the line
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- C_d and C_u (half page jumps) keep the cursor in the middle
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- when jumping to search matches, keep cursor in the middle
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- paste without losing paste buffer
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
