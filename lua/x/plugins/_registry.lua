28876fcfde8645e3ef17664b53527bc7df2b3750
confusedme6 <aljpc+confusedme6@pm.me>
2022-12-27 16:12:25 +0100

    change plugin file names

--- lua/x/plugins/_registry.lua
+++ lua/x/plugins/_registry.lua
@@ -1,20 +1,28 @@
-local fuzzyfinder = require "x.plugins.fuzzyfinder"
-local ast = require "x.plugins.ast"
+local telescope = require "x.plugins.telescope"
+local treesitter = require "x.plugins.treesitter"
 local themes = require "x.plugins.themes"
-local git = require "x.plugins.git"
-local addonsmanager = require "x.plugins.addonsmanager"
-local lsp = require "x.plugins.lsp"
-local autocompletion = require "x.plugins.autocompletion"
-local snippets = require "x.plugins.snippets"
+local fugitive = require "x.plugins.fugitive"
+local mason = require "x.plugins.mason"
+local lspconfig = require "x.plugins.lspconfig"
+local nvimcmp = require "x.plugins.nvimcmp"
+local luasnip = require "x.plugins.luasnip"
 
 
 return {
-    fuzzyfinder,
-    ast,
+    -- fuzzy finder
+    telescope,
+    -- ast
+    treesitter,
+    -- themes
     themes,
-    git,
-    addonsmanager,
-    lsp,
-    autocompletion,
-    snippets,
+    -- git client
+    fugitive,
+    -- addons manager
+    mason,
+    -- lsp-config
+    lspconfig,
+    -- nvimcmp
+    nvimcmp,
+    -- luasnip
+    luasnip,
 }
