42550f6931fdd02f6442cfe1eeccfdf6f9fe36e9
confusedme6 <aljpc+confusedme6@pm.me>
2022-12-27 16:05:01 +0100

    change some stuff in set.lua

--- lua/x/config/set.lua
+++ lua/x/config/set.lua
@@ -4,15 +4,19 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+
+-- indentation --
+-- https://vim.fandom.com/wiki/Indenting_source_code
 vim.opt.expandtab = true
-
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
+vim.opt.foldlevel = 0
 
+-- disable line wrapping
 vim.opt.wrap = false
 
+-- disable swapfile, enable undo file
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
