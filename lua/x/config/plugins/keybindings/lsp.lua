e19142ac1ba68555f4651a97680a173eef3ebbdf
confusedme6 <aljpc+confusedme6@pm.me>
2023-01-06 19:01:52 +0100

    modified telescope keybindings

--- lua/x/config/plugins/keybindings/lsp.lua
+++ lua/x/config/plugins/keybindings/lsp.lua
@@ -1,15 +1,13 @@
+local builtin = require("telescope.builtin")
 return function(client, bufnr)
   local bufopts = { remap = false, buffer = bufnr }
 
-  vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, bufopts)
+  --vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, bufopts)
   vim.keymap.set("n", "dp", vim.diagnostic.goto_prev, bufopts)
   vim.keymap.set("n", "dn", vim.diagnostic.goto_next, bufopts)
   vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, bufopts)
 
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
   vim.keymap.set("n", "gdc", vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set("n", "gtd", vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
   vim.keymap.set("n", "H", vim.lsp.buf.hover, bufopts)
   vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, bufopts)
   --vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
@@ -19,6 +17,15 @@ return function(client, bufnr)
   --end, bufopts)
   vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
   vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set("n", "<leader>rr", vim.lsp.buf.references, bufopts)
+
   vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, bufopts)
+
+  -- supercharging with telescope
+  vim.keymap.set("n", "<leader>d", builtin.diagnostics, bufopts)
+
+  -- there's also incoming calls and outgoing calls if i like that
+  vim.keymap.set("n", "<leader>rr", builtin.lsp_references, bufopts)
+  vim.keymap.set("n", "gd", builtin.lsp_definitions, bufopts)
+  vim.keymap.set("n", "gtd", builtin.lsp_type_definitions, bufopts)
+  vim.keymap.set("n", "gi", builtin.lsp_implementations, bufopts)
 end
